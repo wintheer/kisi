1fb69b3d211bf7d83f21e9dab8ee4d57176e7290
FWinther <fwi@webshipper.com>
2023-02-11 17:28:15 +0100

    Add a simple button component.

---
+++ components/button.vue
@@ -0,0 +1,38 @@
+<template>
+  <button @click="(event) => $emit('click', event)">{{ label }}</button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    label: {
+      type: String,
+      default: 'button'
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../static/assets/main.scss";
+
+button {
+  padding: 6px 20px;
+  color: $white;
+  font-size: inherit;
+  background-color: $primary-color;
+  border: none;
+  border-radius: 20px;
+  transition: background-color .2s;
+  cursor: pointer;
+
+  &:hover {
+    background-color: darken($primary-color, 10%);
+  }
+
+  &:active {
+    background-color: darken($primary-color, 5%);
+  }
+}
+</style>

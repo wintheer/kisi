a4c81acefb4733f05a164fb3e1262c23d099a0f9
FWinther <fwi@webshipper.com>
2023-02-11 17:27:32 +0100

    Implemented card with overlay and hover effects.

--- components/card.vue
+++ components/card.vue
@@ -1,8 +1,13 @@
 <template>
-  <div @mouseenter="hovering = true" @mouseleave="hovering = false">
-    <img :src="backgroundImagePath" alt="background image" />
-    <slot name="header"></slot>
-    <div>
+  <div class="relative" @mouseenter="hovering = true" @mouseleave="hovering = false">
+    <div class="image-container">
+      <div class="image-overlay"></div>
+      <img class="image" :src="backgroundImagePath" alt="background image" />
+    </div>
+    <div class="header">
+      <slot name="header"></slot>
+    </div>
+    <div class="hover-container" :class="{ hidden: !hovering }">
       <slot name="description"></slot>
     </div>
   </div>
@@ -30,4 +35,56 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "../static/assets/main.scss";
+.image {
+  width: 100%;
+  height: inherit;
+  border-radius: 4px;
+
+  object-fit: cover;
+
+  &-overlay {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: 1;
+    background-color: rgba(#000, .2);
+  }
+}
+
+.image-container {
+  height: 100%;
+}
+
+.header {
+  position: absolute;
+  top: 10px;
+  left: 20px;
+  z-index: 3;
+}
+
+.hover-container {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 2;
+  padding-top: 80px;
+  padding-left: 20px;
+  background-color: $card-hover;
+  border-radius: 4px;
+  transition: opacity .2s;
+  opacity: 1;
+
+  &.hidden {
+    opacity: 0;
+  }
+
+  & span {
+    padding-top: 50px;
+  }
+}
 </style>

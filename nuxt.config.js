1a5cc5cba605597f87272ba09c8ebdbc5b5f8771
FWinther <fwi@webshipper.com>
2023-02-13 15:34:20 +0100

    Attempt at displaying something different than just the README

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,9 @@ export default {
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  router: {
+    nase: '/kisi/'
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
